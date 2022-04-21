d729b2bad2b76facc371490b20d2176ea4fac679
BeYou829 <97719110+BeYou829@users.noreply.github.com>
2022-04-21 12:53:31 -0400

    Update Program.cs 0.01
    xD

--- I A E A proyecto tercer parcial/Program.cs
+++ I A E A proyecto tercer parcial/Program.cs
@@ -8,8 +8,42 @@ namespace I_A_E_A_proyecto_tercer_parcial
 {
     internal class Program
     {
+        
+        
         static void Main(string[] args)
         {
+           //Aquí van las variables
+            
+            //Mientras
+            while (opcion != 5)
+            {
+                //Menú
+                
+                //Captura la opción por teclado
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                        case1:
+                        //Capturar el valor
+                        Break;
+                        
+                        case2:
+                        //ProcedimientoTabladeMultiplicar
+                        Break;
+                        
+                        case3:
+                        //Procedimientonúmeroprimo
+                        Break;
+                        
+                        case4:
+                        //Procedimientomatriz
+                        Break;
+                        
+                        case5:
+                        //Finalización
+                        Break;
+                }
+            }
         }
     }
 }
